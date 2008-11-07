df073f6ab4249e7f31a9ca00aebbc7759183b38b
chris.canal@scotalt.net <chris.canal@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2008-11-07 11:29:05 +0000

    Added share CommonAssemblyInfo.cs to standardize version across the project

---
+++ trunk/source/Horn/CommonAssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("scotalt.net")]
+[assembly: AssemblyProduct("HORN")]
+[assembly: AssemblyCopyright("Copyright © scotalt.net 2008")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyVersion("0.0.0.*")]
