5adf595c8a62406224a2d5472fd216f1a4ba25ff
chris.canal@scotalt.net <chris.canal@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2008-11-09 01:20:52 +0000

    Added Integration base class for integration tests and extracted the relevant parts of the Specification class into it's own class so it can be used in Integration and Specification

--- source/Horn/Horn.Spec.Framework/Specification.cs
+++ source/Horn/Horn.Spec.Framework/Specification.cs
@@ -1,28 +1,9 @@
-using System;
 using Rhino.Mocks;
 
-public abstract class Specification : IDisposable
+public abstract class Specification : TestBase
 {
-    protected Specification()
-    {
-        Before_each_spec();
-        Because();
-    }
-
-    protected abstract void Because();
-
-    protected virtual void Before_each_spec() { }
-
-    protected virtual void After_each_spec() { }
-
     protected static T CreateStub<T>() where T : class
     {
         return MockRepository.GenerateStub<T>();
-    }
-
-
-    public void Dispose()
-    {
-        After_each_spec();
     }
 }
