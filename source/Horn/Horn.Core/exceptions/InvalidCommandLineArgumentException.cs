f354fd35495693ef1635c0d7cef4bcb12b2f0d52
dagda1@scotalt.net <dagda1@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2008-11-12 11:03:16 +0000

    Added SwitchParser and CmdLineSpecification for testing

---
+++ source/Horn/Horn.Core/exceptions/InvalidCommandLineArgumentException.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Horn.Core
+{
+    [global::System.Serializable]
+    public class InvalidCommandLineArgumentException : Exception
+    {
+
+        public InvalidCommandLineArgumentException() { }
+
+        public InvalidCommandLineArgumentException(string message) : base(message) { }
+        
+        public InvalidCommandLineArgumentException(string message, Exception inner) : base(message, inner) { }
+        
+        protected InvalidCommandLineArgumentException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
