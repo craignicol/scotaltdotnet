7e20d9a464f8e4b11a5310b572b70c07423bad70
dagda1@scotalt.net <dagda1@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2008-11-15 13:07:05 +0000

    Default PackageTree now created from the cmd line if it does not exist.  A bit of a hack but will allow further development.

--- trunk/source/Horn/Horn.Core.Spec/Unit/DirectoryStructureSpecificationBase.cs
+++ trunk/source/Horn/Horn.Core.Spec/Unit/DirectoryStructureSpecificationBase.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using Horn.Core.PackageStructure;
 
 namespace Horn.Core.Spec.Unit
 {
@@ -14,33 +15,9 @@ namespace Horn.Core.Spec.Unit
         {
             root = string.Format("{0}\\{1}\\", AppDomain.CurrentDomain.BaseDirectory, Environment.UserName);
 
-            CreateDirectory(root);
-
-            var distros = string.Format("{0}{1}\\", root, "distros");
-
-            CreateDirectory(distros);
-
-            var horn = string.Format("{0}{1}\\", distros, "horn");
-
-            CreateDirectory(horn);
-
-            var destinationBuildFile = string.Format("{0}{1}", horn, TEST_BUILD_FILE_NAME);
-
-            if (File.Exists(destinationBuildFile))
-                return;
-
             var sourceBuildFile = string.Format("{0}{1}\\{2}", AppDomain.CurrentDomain.BaseDirectory.ToLower().Replace("bin\\debug", ""), "BuildConfigs\\Horn", TEST_BUILD_FILE_NAME);
 
-            if (!File.Exists(sourceBuildFile))
-                throw new FileNotFoundException("Test horn boo file does not exist");
-
-            File.Copy(sourceBuildFile, destinationBuildFile);
-        }
-
-        private void CreateDirectory(string root)
-        {
-            if (!Directory.Exists(root))
-                Directory.CreateDirectory(root);
-        }        
+            PackageTree.CreateDefaultTreeStructure(root, sourceBuildFile);
+        }       
     }
 }
