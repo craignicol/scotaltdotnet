4af0b5c5896042190f4832a91731efd6f4809ca8
dagda1@scotalt.net <dagda1@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2008-11-15 20:50:55 +0000

    Added IPackageCommand contract and a PackageBuilder to coordinate the install process.

--- trunk/source/Horn/Horn.Core.Spec/Unit/PackageCommands/PackageCommandSpecificationBase.cs
+++ trunk/source/Horn/Horn.Core.Spec/Unit/PackageCommands/PackageCommandSpecificationBase.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace Horn.Core.Spec.Unit.PackageCommands
 {
-    public abstract class PackageBuilderSpecificationBase : Specification
+    public abstract class PackageCommandSpecificationBase : Specification
     {
         protected IDictionary<string, IList<string>> switches = new Dictionary<string, IList<string>>();
 
