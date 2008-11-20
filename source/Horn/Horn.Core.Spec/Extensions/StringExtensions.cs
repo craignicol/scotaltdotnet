94613a4d91c73c2e4423307bf73c72dc0800dde7
dagda1@scotalt.net <dagda1@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2008-11-20 21:05:32 +0000

    Added NAnt scripts that build and run the tests

--- source/Horn/Horn.Core.Spec/Extensions/StringExtensions.cs
+++ source/Horn/Horn.Core.Spec/Extensions/StringExtensions.cs
@@ -4,10 +4,7 @@ namespace Horn.Core.Spec.Extensions
     {
         public static string ResolvePath(this string root)
         {
-            if(root.IndexOf("3.5") == 1)
-                return root.Replace("bin\\debug", "").Replace("build\\net-3.5\\debug", "");
-
-            return root + "\\";
+            return (root.IndexOf("3.5") == 1) ? root.Replace("bin\\debug", "") : root + "\\";
         }
     }
 }
