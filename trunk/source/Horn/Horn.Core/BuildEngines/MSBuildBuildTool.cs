ea895194febde3e6c040feef5c78e2e612ae5aad
dagda1@scotalt.net <dagda1@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2008-11-30 17:35:57 +0000

    Attempt at building the source from MSBuild.  Test fails.

--- trunk/source/Horn/Horn.Core/BuildEngines/MSBuildBuildTool.cs
+++ trunk/source/Horn/Horn.Core/BuildEngines/MSBuildBuildTool.cs
@@ -1,12 +1,39 @@
-using Horn.Core.dsl;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using log4net;
 
 namespace Horn.Core
 {
     public class MSBuildBuildTool : IBuildTool
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof (MSBuildBuildTool));
+
+        public static string MSBuildPath { get; private set; }
+
         public void Build(string pathToBuildFile)
         {
-            
+            var args =
+                string.Format(
+                    "\"{0}\" /p:OutputPath=\"{1}\"  /p:TargetFrameworkVersion=v3.5 /p:NoWarn=1591 /consoleloggerparameters:Summary",
+                    pathToBuildFile, Path.GetDirectoryName(pathToBuildFile));
+
+            try
+            {
+                Process.Start(MSBuildPath, args);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex);
+
+                throw;
+            }
+        }
+
+        static MSBuildBuildTool()
+        {
+            MSBuildPath = Path.Combine(RuntimeEnvironment.GetRuntimeDirectory(), "MSBuild.exe");
         }
     }
 }
