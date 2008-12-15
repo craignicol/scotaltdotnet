eb9a1cb42ab28b9a88c37656c7813d04e577b2c7
dagda1@scotalt.net <dagda1@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2008-12-15 19:57:50 +0000

    Deleting old source location folder

--- trunk/hornbuild.bat
+++
@@ -1,3 +0,0 @@
-..\lib\nant\nant -t:net-3.5 -buildfile:Horn.build -D:sign=false -D:testrunner=NUnit -D:common.testrunner.enabled=true  -D:environment=uat -D:common.testrunner.failonerror=true -D:build.msbuild=true
-
-PAUSE
