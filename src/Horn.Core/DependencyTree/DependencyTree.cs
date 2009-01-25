1fed1085c44d1518a7c9d34496bfb35c43a3b68d
craig.nicol <craig.nicol@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2009-01-25 03:22:59 +0000

    Circular dependencies no longer triggered for shared dependencies

--- src/Horn.Core/DependencyTree/DependencyTree.cs
+++ src/Horn.Core/DependencyTree/DependencyTree.cs
@@ -8,33 +8,107 @@ namespace Horn.Core.DependencyTree
 
     public class DependencyTree : IDependencyTree
     {
+        private class BuildTree
+        {
+            private IList<BuildTree> Children;
+            private BuildTree Parent;
+            private IPackageTree Node;
+            public BuildTree(IPackageTree node)
+                : this(node, new List<BuildTree>(), null)
+		{
+		}
+            public BuildTree(IPackageTree node, BuildTree parent)
+		: this(node, new List<BuildTree>(), parent)
+		{
+		}
+            public BuildTree(IPackageTree node, IList<BuildTree> children)
+		: this(node, children, null)
+		{
+		}
+            public BuildTree(IPackageTree node, IList<BuildTree> children, BuildTree parent)
+            {
+                Parent = parent;
+                Children = children;
+                Node = node;                
+            }
+
+            public BuildTree AddChild(IPackageTree node) 
+            {
+                Children.Add(new BuildTree(node, this));
+                return this;
+            }
+
+            private IList<IPackageTree> GetAncestors(IList<IPackageTree> ancestorList)
+            {
+                ancestorList.Add(Node);
+                if (Parent != null)
+                    return Parent.GetAncestors(ancestorList);
+                else
+                    return ancestorList;
+            }
+
+            public IList<IPackageTree> GetAncestors()
+            {
+                return GetAncestors(new List<IPackageTree>());
+            }
+
+            private IList<IPackageTree> GetBuildList(IList<IPackageTree> buildList)
+            {
+                foreach (BuildTree buildTree in Children)
+                {
+                    buildTree.GetBuildList(buildList);
+                }
+                buildList.Add(Node);
+                return buildList;
+            }
+
+            public IList<IPackageTree> GetBuildList()
+            {
+                return GetBuildList(new List<IPackageTree>());
+            }
+        }
 
         public DependencyTree(IPackageTree packageTree)
         {
             PackageTree = packageTree;
-            BuildList = new List<IPackageTree>();
 
             CalculateDependencies(PackageTree);
         }
 
         public IPackageTree PackageTree { get; private set; }
+        private BuildTree buildTree;
         public IList<IPackageTree> BuildList
         {
-            get;
-            private set;
+            get
+            {
+            		return buildTree.GetBuildList();
+            }
         }
 
         private void CalculateDependencies(IPackageTree packageTree)
         {
-            if (BuildList.Contains(packageTree))
-            {
-                throw new CircularDependencyException(packageTree.Name);
-            }
-            // Insert dependencies before parents
-            BuildList.Insert(0, packageTree);
+            buildTree = CalculateDependencies(packageTree, null);
+        }
 
+        private BuildTree CalculateDependencies(IPackageTree packageTree, BuildTree tree)
+        {
+            if (tree == null)
+            {
+                tree = new BuildTree(packageTree);
+            }
+            else
+            {
+                if (tree.GetAncestors().Contains(packageTree))
+                {
+                    throw new CircularDependencyException(packageTree.Name);
+                }
+                // Insert dependencies before parents
+                tree.AddChild(packageTree);
+            }
             packageTree.GetBuildMetaData().BuildEngine.Dependencies.ForEach(
-                dependency => CalculateDependencies(PackageTree.Retrieve(dependency.Name)));
+                dependency => CalculateDependencies(PackageTree.Retrieve(dependency.Name), tree));
+
+            return tree;
 
         }
     }
