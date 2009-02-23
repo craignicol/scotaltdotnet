cf60abfc87da29166fd17d0f709aa04af7e526aa
dagda1970 <dagda1970@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2009-02-23 18:50:08 +0000

    Added rubydsl branch

---
+++ src/Horn.Dsl.Specificatioin/DslReaderSpecs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using IronRuby;
+using Microsoft.Scripting.Hosting;
+using Xunit;
+
+namespace Horn.Dsl.Specificatioin
+{
+    public class When_given_a_dsl_file_path : Specification
+    {
+        private string buildFile;
+
+        protected override void Because()
+        {
+            buildFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "build.rb");
+        }
+
+        [Fact]
+        public void Then_the_dsl_is_parsed()
+        {            
+            
+            var engine = Ruby.CreateEngine();
+
+            engine.ExecuteFile(buildFile);
+
+            var code = String.Format("{0}.new.method :{1}", "Builder", "do_it");
+
+            var action = engine.CreateScriptSourceFromString(code).Execute();
+
+            var result = engine.Operations.Call(action);
+        }
+    }
+}
