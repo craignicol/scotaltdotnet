aaa80111f56420bfef45abe4d914c8d253be6144
dagda1970 <dagda1970@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2009-02-25 07:31:18 +0000

    Added test to try and return a CLR type from IronRuby

--- src/Horn.Dsl.Specificatioin/BuildConfigs/build.rb
+++ src/Horn.Dsl.Specificatioin/BuildConfigs/build.rb
@@ -1,8 +1,11 @@
-$:.unshift(File.dirname(__FILE__) + '/../../lib') 
+$:.unshift(File.dirname(__FILE__) + '/../bin/Debug') 
 require 'mscorlib'
+require 'Horn.Core.DSL.Domain'
 
-class SimpleType
-	def return_string()
-		"This should be returned"
+class MetaDataFactory
+	def return_meta_data()
+		meta = Horn::Core::DSL::Domain::BuildMetaData.new
+		meta.Description = "A description of sorts"
+	    meta
 	end
 end
