2a9b832f9c763af35fd1579b6116ef2286d98025
dagda1970 <dagda1970@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2009-03-02 08:43:36 +0000

    updated the ruby dsl

--- src/Horn.Core.DSL.Domain/BuildMetaData.cs
+++ src/Horn.Core.DSL.Domain/BuildMetaData.cs
@@ -1,7 +1,23 @@
+using System.Collections.Generic;
+
 namespace Horn.Core.DSL.Domain
 {
     public class BuildMetaData
     {
         public string Description { get; set; }
+
+        public List<Dependency> Dependencies { get; set; }
+
+        public BuildMetaData()
+        {
+            Dependencies = new List<Dependency>();
+        }
+    }
+
+    public class Dependency
+    {
+        public int Id { get; set; }
+
+        public string Value { get; set; }
     }
 }
