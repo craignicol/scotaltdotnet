7ca8bdd806345b3a15ccc566ebd81bffbe8b500e
dagda1970 <dagda1970@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2009-03-17 01:46:43 +0000

    BuildMetaData can be instanciated from within the Ruby Dsl

--- branches/rubydsl/src/Horn.Core/dsl/BuildMetadata/BuildMetaData.cs
+++ branches/rubydsl/src/Horn.Core/dsl/BuildMetadata/BuildMetaData.cs
@@ -3,27 +3,22 @@ using Horn.Core.SCM;
 
 namespace Horn.Core.Dsl
 {
-    public class BuildMetaData : IBuildMetaData
+public class BuildMetaData : IBuildMetaData
+{
+
+    public BuildEngine BuildEngine { get; set; }
+
+    public string Description { get; set; }
+
+    public SourceControl SourceControl { get; set; }
+
+    public BuildMetaData(){}
+
+    public BuildMetaData(BaseConfigReader instance)
     {
+        BuildEngine = instance.BuildEngine;
 
-        public BuildEngine BuildEngine { get; set; }
-
-        public string Description { get; set; }
-
-        public SourceControl SourceControl { get; set; }
-
-
-
-        public BuildMetaData(){}
-
-        public BuildMetaData(BaseConfigReader instance)
-        {
-            BuildEngine = instance.BuildEngine;
-
-            SourceControl = instance.SourceControl;
-        }
-
-
-
+        SourceControl = instance.SourceControl;
     }
+}
 }
