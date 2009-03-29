717aa20138f33e3a28a71e6e49e6980e96efe06e
dagda1@scotalt.net <dagda1@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2009-03-29 04:37:06 +0000

    package_tree change

--- branches/rubydsl/package_tree/builders/horn/horn.rb
+++ branches/rubydsl/package_tree/builders/horn/horn.rb
@@ -4,6 +4,7 @@ install :horn do
   description "A .NET build and dependency manager"
   build_with :msbuild, :frameworkVersion35, :buildfile => "src/horn.sln"
   get_from :svn, "http://scotaltdotnet.googlecode.com/svn/trunk/"
+  output "Output"
   
   dependency :log4net =>  "bin/net/2.0/debug"
   #dependency :castle =>   "castle.core"
