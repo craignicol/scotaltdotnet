fa3fe3f42973554a322d0c3f2cd36daf5100327b
dagda1@scotalt.net <dagda1@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2009-04-03 12:41:46 +0000

    Presentation update

--- belfast/ioc_and_testability/ioc_and_testability/Reporter/ReportController.cs
+++ belfast/ioc_and_testability/ioc_and_testability/Reporter/ReportController.cs
@@ -1,31 +1,24 @@
-using System;
 using System.Web.Mvc;
-using System.Web.Routing;
 
 namespace ddd.belfast.ioc
 {
     public class ReportController : Controller
     {
-        private readonly ReportBuilder _reportBuilder;
-        private readonly ReportSender _reportSender;
+        private ReportBuilder _reportBuilder;
+        private ReportSender _reportSender;
 
         public virtual bool SendAuditReports()
         {
+            _reportBuilder = new ReportBuilder();
+            var emailSender = new EmailSender();
+            _reportSender = new ReportSender(emailSender);
+
             foreach(var report in _reportBuilder.CreateReports())
             {
                 _reportSender.Send(report);
             }
 
             return true;
-        }
-
-        public ReportController()
-        {
-            _reportBuilder = new ReportBuilder();
-
-            var emailSender = new EmailSender();
-
-            _reportSender = new ReportSender(emailSender);
         }
     }
 }
