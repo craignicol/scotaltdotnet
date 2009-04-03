4d8446ae7eb7f0af3612efa0be9ca2eacbcc0e37
dagda1@scotalt.net <dagda1@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2009-04-03 14:09:14 +0000

    Presentation update

---
+++ belfast/ioc_and_testability/ioc_and_testability/plan/Contact.cs
@@ -0,0 +1,12 @@
+namespace ddd.belfast.plan
+{
+    public class Contact
+    {
+        public string Email { get; set; }
+
+        public Contact(string email)
+        {
+            Email = email;
+        }
+    }
+}
