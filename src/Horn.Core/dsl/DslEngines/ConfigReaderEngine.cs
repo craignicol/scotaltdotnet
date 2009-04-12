70b270a2c0b643849f95822d1d9ade8b92dc4fc0
dagda1@scotalt.net <dagda1@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2009-04-12 01:44:50 +0000

    Presentation update

--- src/Horn.Core/dsl/DslEngines/ConfigReaderEngine.cs
+++ src/Horn.Core/dsl/DslEngines/ConfigReaderEngine.cs
@@ -5,19 +5,14 @@ namespace Horn.Core.Dsl
 {
     using Boo.Lang.Compiler.Steps;
 
-    public class ConfigReaderEngine : DslEngine
+public class ConfigReaderEngine : DslEngine
+{
+    protected override void CustomizeCompiler(BooCompiler compiler, CompilerPipeline pipeline, string[] urls)
     {
-
-        protected override void CustomizeCompiler(BooCompiler compiler, CompilerPipeline pipeline, string[] urls)
-        {
-            pipeline.Insert(1, new ImplicitBaseClassCompilerStep(typeof(BooConfigReader), "Prepare", "Horn.Core.Dsl"));
-            pipeline.InsertBefore(typeof(ProcessMethodBodiesWithDuckTyping), new RightShiftToMethodCompilerStep());
-            pipeline.Insert(2, new UnderscorNamingConventionsToPascalCaseCompilerStep());
-            pipeline.Insert(3, new UseSymbolsStep());
-            
-        }
-
-
-
+        pipeline.Insert(1, new ImplicitBaseClassCompilerStep(typeof(BooConfigReader), "Prepare", "Horn.Core.Dsl"));
+        pipeline.InsertBefore(typeof(ProcessMethodBodiesWithDuckTyping), new RightShiftToMethodCompilerStep());
+        pipeline.Insert(2, new UnderscorNamingConventionsToPascalCaseCompilerStep());
+        pipeline.Insert(3, new UseSymbolsStep());            
     }
+}
 }
