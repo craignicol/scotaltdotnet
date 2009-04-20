952dee6119599d4b4ea760250d5dfb236b7194dc
dhtmlgod@gmail.com <dhtmlgod@gmail.com@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2009-04-20 16:28:29 +0000

    Removed unused ILog field

--- src/Horn.Core/Dependencies/DependencyCopier.cs
+++ src/Horn.Core/Dependencies/DependencyCopier.cs
@@ -3,13 +3,10 @@ namespace Horn.Core.Dependencies
     using System.Collections.Generic;
     using System.IO;
     using extensions;
-    using log4net;
     using Utils;
 
     public class DependencyCopier : WithLogging
     {
-        private static readonly ILog log = LogManager.GetLogger(typeof(DependencyCopier));
-
         public IEnumerable<string> CopyDependency(FileInfo file, DirectoryInfo destination)
         {
             IEnumerable<string> locationsToCopyTo = destination.Search(file.Name);
