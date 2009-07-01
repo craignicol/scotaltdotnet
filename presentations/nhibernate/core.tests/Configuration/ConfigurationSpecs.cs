0ea356a3a8785c9b2796bc1f39b2956b694923f5
dagda1@scotalt.net <dagda1@scotalt.net@b938e876-9fc1-11dd-b4aa-53559073bf4c>
2009-07-01 20:41:59 +0000

    Base lined the demo project so i can easily revert to a new state.

--- presentations/nhibernate/core.tests/Configuration/ConfigurationSpecs.cs
+++ presentations/nhibernate/core.tests/Configuration/ConfigurationSpecs.cs
@@ -7,6 +7,8 @@ namespace core.tests.cfg
     [TestFixture]
     public class When_nhibernate_is_configured_correctly
     {
+
+
         [Test]
         public void The_we_can_open_a_session()
         {
